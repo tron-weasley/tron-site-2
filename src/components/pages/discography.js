494e1ef0c6a95a7c2e9e8c8f1e5b9ee2961fe6f4
user <user@users-imac.attlocal.net>
2020-05-05 23:17:53 -0500

    Moved images over to the app.

--- src/components/pages/discography.js
+++ src/components/pages/discography.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
-export class Discography extends Component {
+class Discography extends Component {
     render() {
         return (
             <div>
