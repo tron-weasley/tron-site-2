a9a6cc44bbe541d819c3e38149b9dcd8579f899f
user <user@users-imac.attlocal.net>
2020-05-05 20:29:43 -0500

    Beginning to add component syntax to component files. Pseudocoding their contents next.

---
+++ src/components/navbar/social.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class social extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default social
