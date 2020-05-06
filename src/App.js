cc0f6703ef8de5c3399b12f8cadb5318e68ba0d7
user <user@users-imac.attlocal.net>
2020-05-05 20:51:14 -0500

    Successfully sent information from the home component to the site. Bandcamp link doesn't translate like normal html to jsx so I'll have to find a work-around for the home page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './components/pages/home';
 
 function App() {
   return (
     <div className="App">
       <h1>hello!</h1>
+      <p>hellow</p>
+      <Home />
     </div>
   );
 }
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export class home extends Component {
+export class Home extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="home">
+            
+                {/* <div className= "bandcamp">
+                    <iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=2328359174/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/" seamless><a href="http://tronweasley.bandcamp.com/album/magnetic-router-sounds-vol-1">Magnetic Router Sounds Vol. 1 by Tron Weasley</a></iframe>
+                </div> */}
+    
+                <div className="welcome">
+                    <p>Magnetic Router Sounds, Vol. 1 is out NOW!</p>
+                </div> 
             </div>
-        )
+        );
     }
 }
 
-export default home
+export default Home;
